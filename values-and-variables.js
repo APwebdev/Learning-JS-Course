9f200d5a8d93dacc915570fcfc0650ef6fe0a709
Adas <adasdevelopment@gmail.com>
2021-07-02 23:42:49 +0300

    First day of learning JS on the bought udemy course

---
+++ values-and-variables.js
@@ -0,0 +1,40 @@
+// Numbers
+
+3 + 4
+1.5 + 8
+12 - 4.567
+4 * 1.3
+4 / 2 
+25 % 3 
+2 ** 3 // 2 * 2 * 2
+3 - 9 * 2 // Will do multiplication first! Parentesis can be used to modify that.
+
+// NaN & Infinity
+
+0 / 0 // = NaN
+1 + NaN // = NaN
+1 / 0 // = Infinity
+- 1 / 0 // = -Infinity
+// 0 and -0 also exists in Javascript!
+
+// Numbers Quiz
+
+1.5 + 1.5 * 2 // = 4.5.
+(10 % 6) ** 2 // = 16
+200 + 0 / 0 // = NaN
+
+// Variables & Let
+
+let someName = value // Basic Syntax
+
+let hens = 4;
+let roosters = 2;
+hens + roosters // = 6
+
+let hens = 4;
+// A racoon killed a hen.
+hens - 1; // = 3
+hens; // Still 4!
+// To actually change hens:
+hens = hens - 1;
+hens // = 3
