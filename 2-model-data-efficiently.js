bc4844fa935ec31bd81205dccab47b7ae5e30eae
Adas <adasdevelopment@gmail.com>
2021-07-04 13:07:06 +0300

    Boolean

---
+++ 2-model-data-efficiently.js
@@ -0,0 +1,13 @@
+// BOOLEAN 
+
+
+// Booleans are simple True or False values (Yes or No). (1 or 0)
+let isLoggedIn = true;
+let gameOver = false;
+const isWaterWet = true;
+
+// Variables can change type
+let numDonuts = 12; // It's a Number
+numDonuts = false; // now i'ts a Boolean!
+numDonuts = 12987346; // back to number!
+// You probably wouldn't change a number to a Boolean, but you can!
