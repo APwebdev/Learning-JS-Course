f3c08741cfb182c51a1aedca1855c0bdea9ee69d
Adas <adasdevelopment@gmail.com>
2021-07-04 13:18:15 +0300

    Strings

--- 2-model-data-efficiently.js
+++ 2-model-data-efficiently.js
@@ -11,3 +11,18 @@ let numDonuts = 12; // It's a Number
 numDonuts = false; // now i'ts a Boolean!
 numDonuts = 12987346; // back to number!
 // You probably wouldn't change a number to a Boolean, but you can!
+
+
+// STRINGS
+
+
+// Strings in Javascript are pieces of text, or strings of characters. (We wrap them in quotes!)
+
+//Whether you use single or double quotes, just make sure you are consistent!
+let fistName = "Ziggy";
+let lasName = "Canaryface";
+let msg = "Please don't feed the chimps"
+let animal = 'Dumbo Octopus'
+let something = 'he said "Make me do it!" '
+firstName + lastName; // = ZiggyCanaryface
+firstName + " " + lasName; // = Ziggy Canaryface
