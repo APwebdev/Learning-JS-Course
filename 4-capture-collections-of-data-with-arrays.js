e857737da9824f36490abc7e983db938653e0706
Adas <adasdevelopment@gmail.com>
2021-07-07 10:23:19 +0300

    Creating Arrays

--- 4-capture-collections-of-data-with-arrays.js
+++ 4-capture-collections-of-data-with-arrays.js
@@ -1,3 +1,35 @@
 // CREATING ARRAYS
 
 
+/*
+
+Goals
+- Work with Arrays
+- Write Object Literals
+- Understand Reference Types
+- Use common Array/Object methods
+
+*/
+
+/*
+
+Arrays
+Ordered collections of values.
+
+- List of comments on IG post
+- Collection of levels in a game
+- Songs in a playlist
+
+*/
+
+// To make an empty array
+let students = [];
+
+// An array of strings 
+let colors = ['red', 'orange', 'yellow'];
+
+// An array of numbers
+let lottoNums = [19,22,56,12,51]
+
+// A mixed array
+let stuff = [true, 68, 'cat', null];
