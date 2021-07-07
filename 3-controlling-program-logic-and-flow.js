c5fd5e103fe85e94e78fbfbd4d4f6aac6fa60be1
Adas <adasdevelopment@gmail.com>
2021-07-07 10:02:31 +0300

    Switch Statement

--- 3-controlling-program-logic-and-flow.js
+++ 3-controlling-program-logic-and-flow.js
@@ -277,3 +277,54 @@ if(flavor !== 'grape' && flavor !== 'cherry') {
 let x = 7;
 x == 7 || (x === 3 && x > 10); // = true
 (x == 7 || x === 3) && x > 10; // = false
+
+
+// THE SWITCH STATEMENT
+
+
+let day = 3;
+if( day === 1) {
+    console.log("Monday")
+} else if(day === 2) {
+    console.log("Tuesday");
+} else if(day === 3) {
+    console.log("Wednesday");
+} else if(day === 4) {
+    console.log("Thursday");
+} else if(day === 5) {
+    console.log("Friday");
+} else if(day === 6) {
+    console.log("Saturday");
+} else if(day === 7) {
+    console.log("Sunday");
+} else {
+    console.log('Invalid day!')
+}
+
+// Easier way to do "Example 1"
+
+switch(day) {
+    case 1:
+        console.log("Monday");
+        break;
+    case 2:
+        console.log("Tuesday");
+        break;
+    case 3:
+        console.log("Wednesday");
+        break;
+    case 4:
+        console.log("Thursday");
+        break;
+    case 5:
+        console.log("Friday");
+        break;
+    case 6:
+        console.log("Saturday");
+        break;
+    case 7:
+        console.log("Sunday");
+        break;
+    default:
+        console.log('Invalid day!');
+}
